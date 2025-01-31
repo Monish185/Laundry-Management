432818bfe972390ec78e25e08ef9d388122c691f
Monish <23ucs648@lnmiit.ac.in>
2025-01-31 20:09:31 +0530

    changed url

--- Laundry/Laundry/urls.py
+++ Laundry/Laundry/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('laundry/',include('laundryapp.urls'))
+    path('', lambda request: redirect('/laundry/')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
