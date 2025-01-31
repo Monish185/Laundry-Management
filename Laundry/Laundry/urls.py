402cf8787496ee7b4ace74788cf2bf8b83aa14b4
Monish185 <154275354+Monish185@users.noreply.github.com>
2025-01-31 20:15:17 +0530

    Update urls.py added a comma

--- Laundry/Laundry/urls.py
+++ Laundry/Laundry/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 from django.shortcuts import redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('laundry/',include('laundryapp.urls'))
+    path('laundry/',include('laundryapp.urls')),
     path('', lambda request: redirect('/laundry/')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
