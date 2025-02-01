616f7ffe8f294a6b1dee3e034c4ee014f531dcce
Monish <23ucs648@lnmiit.ac.in>
2025-02-01 19:24:44 +0530

    added build file

---
+++ Laundry/build.sh
@@ -0,0 +1,16 @@
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+
+python manage.py makemigrations
+python manage.py migrate
+
+if [[ "$CREATE_SUPERUSER" == "true" ]]; then
+    echo "Creating superuser..."
+    python manage.py createsuperuser \
+        --no-input \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL"
+fi
